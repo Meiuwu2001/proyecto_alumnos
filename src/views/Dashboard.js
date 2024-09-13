b1d0c0958ff264e45da2a4daa575f09d2c86dd27
Samuel Reyna González <118919573+Meiuwu2001@users.noreply.github.com>
2024-09-12 23:44:20 -0600

    implementacion de imagenes al dashboard

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,119 +1,128 @@
-/*!
-
-=========================================================
-* Paper Dashboard React - v1.3.1
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
-
-* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/main/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
-// react plugin used to create charts
-import { Line, Pie } from "react-chartjs-2";
-import hombre from "hombre.png";
-import mujer from "mujer.webp";
 // reactstrap components
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  CardTitle,
-  Row,
-  Col
-} from "reactstrap";
-// core components
-import {
-  dashboard24HoursPerformanceChart,
-  dashboardEmailStatisticsChart,
-  dashboardNASDAQChart
-} from "variables/charts.js";
+import { Card, CardBody, CardFooter, CardTitle, Row, Col } from "reactstrap";
+
+const alumnos = [
+  {
+    nombre: "Gustavo",
+    apellido: "Campagne",
+    imagen: require("../assets/img/alumnos/Campagne.jpg"),
+  },
+  {
+    nombre: "Bernardo",
+    apellido: "Castañeda",
+    imagen: require("../assets/img/alumnos/castañeda.jpg"),
+  },
+  {
+    nombre: "Diego",
+    apellido: "Gonzalez",
+    imagen: require("../assets/img/alumnos/GonzálezHerrera.png"),
+  },
+  {
+    nombre: "Jonathan",
+    apellido: "Gurrola",
+    imagen: require("../assets/img/alumnos/GurrolaAntuna.jpg"),
+  },
+  {
+    nombre: "Paola",
+    apellido: "Herrera",
+    imagen: require("../assets/img/alumnos/Herrera_Ortiz.jpg"),
+  },
+  {
+    nombre: "Graciela",
+    apellido: "Leon",
+    imagen: require("../assets/img/alumnos/Leon_Quintanaa.jpeg"),
+  },
+  {
+    nombre: "Luis",
+    apellido: "Luna",
+    imagen: require("../assets/img/alumnos/luna.jpeg"),
+  },
+  {
+    nombre: "Miguel",
+    apellido: "Manzanilla",
+    imagen: require("../assets/img/alumnos/manzanilla.jpg"),
+  },
+  {
+    nombre: "Marlenne",
+    apellido: "",
+    imagen: require("../assets/img/alumnos/Marlenne.png"),
+  },
+  {
+    nombre: "Luis",
+    apellido: "Ontiveros",
+    imagen: require("../assets/img/alumnos/ontiveros.png"),
+  },
+  {
+    nombre: "Samuel",
+    apellido: "Reyna",
+    imagen: require("../assets/img/alumnos/reyna.jpg"),
+  },
+  {
+    nombre: "Diego",
+    apellido: "Rios",
+    imagen: require("../assets/img/alumnos/rios.jpeg"),
+  },
+  {
+    nombre: "Daniel",
+    apellido: "Sanchez",
+    imagen: require("../assets/img/alumnos/Sanchez_Daniel.jpeg"),
+  },
+  {
+    nombre: "Alma",
+    apellido: "Soto",
+    imagen: require("../assets/img/alumnos/sotocervantes.jpg"),
+  },
+  {
+    nombre: "Victor",
+    apellido: "Soto",
+    imagen: require("../assets/img/alumnos/SotoGraciano.jpg"),
+  },
+  {
+    nombre: "Edgar",
+    apellido: "Vallejo",
+    imagen: require("../assets/img/alumnos/vallejo.jpg"),
+  },
+];
 
 function Dashboard() {
   return (
     <>
       <div className="content">
         <Row>
-          <Col lg="3" md="6" sm="6">
-            <Card className="card-stats">
-              <CardBody>
-                <Row>
-                  <Col md="4" xs="5">
-                    <div className="icon-big text-center icon-warning">
-                      <i className="nc-icon nc-globe text-warning" />
-                    </div>
-                  </Col>
-                  <Col md="8" xs="7">
-                    <div className="numbers">
-                      <p className="card-category">ALUMNO 1</p>
-                      <CardTitle tag="p">JUAN PEREZ</CardTitle>
-                      <p />
-                    </div>
-                  </Col>
-                </Row>
-              </CardBody>
-              <CardFooter>
-                <hr />
-                <div className="stats">
-                </div>
-                <a
-                  href="/"
-                  className="simple-text logo-mini"
-                >
-                  <div className="logo-img">
-                    <img src={hombre} alt="react-logo" />
-                  </div>
-                </a>
-              </CardFooter>
-            </Card>
-          </Col>
-
-          <Col lg="3" md="6" sm="6">
-            <Card className="card-stats">
-              <CardBody>
-                <Row>
-                  <Col md="4" xs="5">
-                    <div className="icon-big text-center icon-warning">
-                      <i className="nc-icon nc-globe text-warning" />
-                    </div>
-                  </Col>
-                  <Col md="8" xs="7">
-                    <div className="numbers">
-                      <p className="card-category">ALUMNO 2</p>
-                      <CardTitle tag="p">SANDRA LÓPEZ</CardTitle>
-                      <p />
-                    </div>
-                  </Col>
-                </Row>
-              </CardBody>
-              <CardFooter>
-                <hr />
-                <div className="stats">
-                </div>
-                <a
-                  href="/"
-                  className="simple-text logo-mini"
-                >
-                  <div className="logo-img">
-                    <img src={mujer} alt="react-logo" />
-                  </div>
-                </a>
-              </CardFooter>
-            </Card>
-          </Col>         
-        </Row>
-        <Row>
-          
-          
+          {alumnos.map((alumno, index) => (
+            <Col lg="3" md="6" sm="6" key={index}>
+              <Card className="card-stats">
+                <CardBody>
+                  <Row>
+                    <Col md="4" xs="5">
+                      <div className="icon-big text-center icon-warning">
+                        <img
+                          src={alumno.imagen}
+                          alt={`${alumno.nombre} ${alumno.apellido}`}
+                          className="img-fluid rounded-circle"
+                          style={{ maxWidth: "100px" }}
+                        />
+                      </div>
+                    </Col>
+                    <Col md="8" xs="7">
+                      <div className="numbers">
+                        <p className="card-category">Alumno</p>
+                        <CardTitle tag="p">
+                          {alumno.nombre} {alumno.apellido}
+                        </CardTitle>
+                        <p />
+                      </div>
+                    </Col>
+                  </Row>
+                </CardBody>
+                <CardFooter>
+                  <hr />
+                  <div className="stats"></div>
+                </CardFooter>
+              </Card>
+            </Col>
+          ))}
         </Row>
       </div>
     </>
